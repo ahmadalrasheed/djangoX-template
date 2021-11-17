f9134fd9726f9ded38e57d5dad8b69e768b4b754
ahmadalrasheed <alrasheedahmad97@gmail.com>
2021-11-17 23:40:11 +0200

    feat : add crud app with styling

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your models here.
+
+class Snack(models.Model):
+    """
+    This class is declaring the model of the data (thier shape) that we want to add into database
+    """
+
+    title = models.CharField(max_length=64)
+    purchaser = models.ForeignKey(get_user_model(),on_delete=models.CASCADE )
+    snack = models.IntegerField(default=0)
+    description = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('snackdetails' , args=[str(self.pk)])
+
+
+
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,40 @@
 from django.views.generic import TemplateView
+from django.db import models
+from django.shortcuts import render
+from .models import Snack
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 
+# Create your views here.
+class SnackListView(ListView):
+    template_name='pages/home.html'
+    model = Snack
 
-class HomePageView(TemplateView):
-    template_name = 'pages/home.html'
+class SnackDetailView(DetailView):
+    template_name='snack_detail_view.html'
+    model = Snack
+    context_object_name= 'snack_details'
+
+class SnackCreateView(CreateView):
+    template_name='snack_create_view.html'
+    model = Snack
+    fields = ['title' , 'purchaser', 'description']
+
+class SnackUpdateView(UpdateView):
+    template_name='snack_update_view.html'
+    model = Snack
+    fields = ['title' , 'purchaser', 'description']
 
 
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
+
+
+
+
+
